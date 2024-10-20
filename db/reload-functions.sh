338d89490ab3ef14dc66651ad85dea298e323998
Robert Millard <robert.millard@getmoss.com>
2024-10-20 14:23:47 +0200

    tables

--- db/reload-functions.sh
+++ db/reload-functions.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
-# RELOAD SQL FUNCTIONS WITHOUT LOSING DATA
 
-# Assumes you have already created database & tables:
-# createuser srs
-# createdb -O srs srs
-# psql -U srs -d srs -f tables.sql
+RELOAD_TABLES=false
+
+while [ "$1" != "" ]; do
+    case $1 in
+        --tables )        RELOAD_TABLES=true
+                          ;;
+        * )               echo "Invalid option: $1"
+                          exit 1
+    esac
+    shift
+done
 
 psql -h localhost -U srs -d srs -c "drop schema srs cascade"
 psql -h localhost -U srs -d srs -c "set search_path = srs,public"
-psql -h localhost -U srs -d srs -f ./db/tables.sql
+if [ "$RELOAD_TABLES" = true ]; then
+    echo "Reloading tables..."
+    psql -h localhost -U srs -d srs -f ./db/tables.sql
+else
+    psql -h localhost -U srs -d srs -c "create schema srs"
+    echo "Skipping table reload..."
+fi
+
 psql -h localhost -U srs -d srs -f ./db/functions.sql
 psql -h localhost -U srs -d srs -f ./db/api.sql
 
